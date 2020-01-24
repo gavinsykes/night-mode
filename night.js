aa0804fb35eb4d1f73aa727520c148b0252eec5b
Gavin Sykes <gavin@provu.co.uk>
2020-01-24 22:20:59 +0000

    Create night.js

---
+++ night.js
@@ -0,0 +1,32 @@
+const keyElems = {
+  html        : document.querySelector('html'),
+  nightButton : document.querySelector('.night-button')
+};
+function toggleNight() {
+  isNight() ? setNight(false) : setNight(true);
+}
+function isNight() {
+  return keyElems.html.classList.contains('night');
+}
+function setNight(night) {
+  if (night) {
+    keyElems.html.classList.add('night');
+    keyElems.nightButton.textContent = `Day Mode`;
+  } else {
+    keyElems.html.classList.remove('night');
+    keyElems.nightButton.textContent = `Night Mode`;
+  }
+}
+function checkNightOnLoad() {
+  let h = new Date().getHours();
+  console.log(h);
+  if (h < 7 || h > 18) {
+    setNight(true);
+  } else {
+    setNight(false);
+  }
+}
+document.addEventListener('DOMContentLoaded',() => {
+  checkNightOnLoad();
+  keyElems.nightButton.addEventListener('click',toggleNight);
+});
